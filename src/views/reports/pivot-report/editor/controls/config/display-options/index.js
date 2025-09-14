14d9df8280052291108d4ac2b2bc766a9da534f8
Rahul024.y <ryadav122011@gmail.com>
2025-09-14 20:52:42 +0530

    This is my commit message describing the changes.

--- src/views/reports/pivot-report/editor/controls/config/display-options/index.js
+++
@@ -1,9 +0,0 @@
-import DisplayOption from './DefaultOptions';
-import DateRangeFieldOptions from './DateRangeFieldOptions';
-
-const displayOptionComponent = {
-    any: DisplayOption,
-    'date-range': DateRangeFieldOptions
-};
-
-export default displayOptionComponent;
